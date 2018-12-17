5b7d9a04fefef31f5d7d462e9bca7fd4b8686709
coren <coren114@gmail.com>
2018-12-16 22:49:07 -0500

    carpeta img /  parametros

---
+++ PrototiposPoltran/ParametrosSistema.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrototiposPoltran
+{
+    /// <summary>
+    /// Lógica de interacción para ParametrosSistema.xaml
+    /// </summary>
+    public partial class ParametrosSistema : UserControl
+    {
+        public ParametrosSistema()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
